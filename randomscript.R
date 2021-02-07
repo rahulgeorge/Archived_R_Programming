9b0281df2da26d84989827b6abae8db05d4e6052
ashlykoshy <koshyashly@gmail.com>
2021-02-07 11:01:41 +0530

    checking in updated random script

--- randomscript.R
+++ randomscript.R
@@ -1,6 +1,14 @@
 #x <- c(1,2, NA, NaN, 10, 3)
 a <- 0/0
 b <- 2
-y <- matrix(list(c(T,F), c("F","B"), b), 3,1)
+y <- matrix(list (F, "B", b), 3,1)
 
-x <- data.frame(food = 1:5, bar = list(T,"T",1.2,3+2i,4))
+x <- data.frame(food = 1:5, bar = c(T,T,F,F,F))
+#x <- 1:3
+#names(x) <- list(3, T, "bye")
+#z <- as.integer(names(x))
+#a <- 1:3
+#names(a) <- z
+
+dimnames(y) <- list(c("yea", "yep", "yaay"), "wow")
+dimnames(x) <- list(c("name", "place", "animal","thing","howold"), c("fool", "bar"))
